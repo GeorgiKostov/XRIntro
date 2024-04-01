ee6b136b8859581f9bc7568edf5cf75f4f3dd0dc
Georgi Kostov <georgikostov1337@gmail.com>
2024-04-01 17:19:02 +0200

    Add canvas follow script

--- Assets/Scripts/CanvasFollow.cs
+++ Assets/Scripts/CanvasFollow.cs
@@ -4,34 +4,54 @@ using UnityEngine;
 public class CanvasFollow : MonoBehaviour
 {
     public GameObject mainCamera;
-
     public float lerpSpeed = 0.1f;
-
-    public float distanceFromPlayer = 1;
-
+    public float predefinedDistance = 1;
     public bool moveWithPlayer;
-
     public bool rotateWithPlayer;
+    public bool scaleFromPlayer;
+    [Header("Scaling")]
+    public float defaultScale = 0.001f;
+    public float minDistanceThreshold = 0.3f;
+    public float maxDistance = 5.0f;
+    public float distance;
 
     void Update()
     {
         // Ensure we have references to the Canvas and Camera
-        if (this.transform == null || mainCamera == null)
+        if (mainCamera == null)
         {
-            Debug.LogError("Canvas or Main Camera not assigned in the inspector!");
+            Debug.LogError("Main Camera not assigned in the inspector!");
             return;
         }
-
         if (moveWithPlayer)
         {
-            Vector3 targetPosition = mainCamera.transform.position + mainCamera.transform.forward * distanceFromPlayer;
+            // use main camera position and forward vector to calculate canvas position
+            Vector3 targetPosition = mainCamera.transform.position + mainCamera.transform.forward * predefinedDistance;
             transform.position = Vector3.Lerp(transform.position, targetPosition, lerpSpeed);
         }
-
         if (rotateWithPlayer)
         {
+            // look at main camera and flip canvas forward vector
             transform.LookAt(new Vector3(mainCamera.transform.position.x, transform.position.y, mainCamera.transform.position.z));
             transform.forward *= -1;
+        }
+        if (scaleFromPlayer)
+        {
+            // Calculate distance
+            distance = Vector3.Distance(transform.position, mainCamera.transform.position);
+
+            // Check if we are under or over threshold and use preset
+            if (distance < minDistanceThreshold)
+            {
+                distance = minDistanceThreshold;
+            }
+            if (distance > maxDistance)
+            {
+                distance = maxDistance;
+            }
+            // Apply the scale
+            float scale = distance * defaultScale;
+            transform.localScale = new Vector3(scale, scale, scale);
         }
     }
 }
